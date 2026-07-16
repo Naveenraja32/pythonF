1bebcb816092862e07a458c2e1a697b1f4cb7fb5
Naveenraja32 <naveenrajaspt3@gmail.com>
2026-07-16 17:36:32 +0530

    validation

--- Strings/coll.py
+++ Strings/coll.py
@@ -21,14 +21,38 @@ from collections import *
 # data.popitem(last=False)
 # print(data)
 
-data=deque([5,1,3,2])
-data.rotate(2)
-print(data)
-data.appendleft(4)
-print(data)
-data.extendleft([6,7])
-print(data)
-print(data.pop())
-print(data.popleft())
-data.extend([8,9])
-print(data)
+# data=deque([5,1,3,2])
+# data.rotate(2)
+# print(data)
+# data.appendleft(4)
+# print(data)
+# data.extendleft([6,7])
+# print(data)
+# print(data.pop())
+# print(data.popleft())
+# data.extend([8,9])
+# print(data)
+
+
+str="[{()}]"
+data=[]
+for i in str:
+    if i in ['(','[','{']:
+        data.append(i)
+    else:
+        if len(data)==0:
+            print("Not Balanced")
+            break
+        else:
+            x=data.pop()
+            if x=='(' and i==')':
+                continue
+            elif x=='[' and i==']':
+                continue
+            elif x=='{' and i=='}':
+                continue
+            else:
+                print("Not Balanced")
+                break
+else:
+    print("Balanced")
