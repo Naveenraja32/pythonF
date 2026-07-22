f719420ab8d11bdf27535923a00ef858eac560e5
Naveenraja32 <naveenrajaspt3@gmail.com>
2026-07-22 17:03:45 +0530

    u2

--- Strings/coll.py
+++ Strings/coll.py
@@ -97,11 +97,8 @@ from collections import *
 #         elif j not in base:break
 #     else:rt.append(i)
 # print(rt)
-  
-# print({chr(x+96):x for x in range(1,27)})
-dt={'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6, 'g': 7, 'h': 8, 'i': 9, 'j': 10, 'k': 11, 'l': 12, 'm': 13, 'n': 14, 'o': 15, 'p': 16, 'q': 17, 'r': 18, 's': 19, 't': 20, 'u': 21, 'v': 22, 'w': 23, 'x': 24, 'y': 25, 'z': 26}
 
-word = 'dbb'
+word = 'axz'
 right = left = 0
 for i in word: right += ord(i)-96
 for i in word:
