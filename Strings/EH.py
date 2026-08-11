f7d84a20f0924cec3c7e1ff4ebed9bdaea57725d
Naveenraja32 <naveenrajaspt3@gmail.com>
2026-08-11 17:52:21 +0530

    up1

--- Strings/EH.py
+++ Strings/EH.py
@@ -1,10 +1,14 @@
 try:
-    number1 , number2 = int(input()) , int(input())
+    number1 , number2 = int(input("Enter the first number: ")) , int(input("Enter the second number: "  ))
     result = number1/number2
 except ZeroDivisionError as zero:
     print(f'{zero} error')
 except Exception as e:
     print(type(e).__name__)
+# except ValueError :
+#     print('Value Error')
+# except ZeroDivisionError:
+#     print('Zero Division Error')
 else:
     print(result)
 finally:
