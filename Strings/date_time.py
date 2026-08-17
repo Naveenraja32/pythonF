a2e3f1589c007cbbba69504294e55d34f3ce2b30
Naveenraja32 <naveenrajaspt3@gmail.com>
2026-08-17 17:44:28 +0530

    upd1

--- Strings/date_time.py
+++ Strings/date_time.py
@@ -1,4 +1,4 @@
-from datetime import datetime, timedelta
+from datetime import datetime, time, timedelta
 from zoneinfo import ZoneInfo
 
 timeZone = ZoneInfo('Asia/Kolkata')
@@ -7,8 +7,17 @@ now = datetime.now(tz=timeZone)
 # Parses the string to a naive datetime, then applies the timezone
 dateB = datetime.strptime('03-08-2026', '%d-%m-%Y').replace(tzinfo=timeZone)
 
+print(datetime.combine(now, time=time(8,59,50)))
+
 # This will print the days/hours passed since August 3rd
 print(now - dateB)
 
 # Fixed the extra parenthesis error here
 print(datetime.strftime(now + timedelta(days=1), '%d-%m-%Y'))
+
+# now = datetime.now().date()
+# try:
+#     print(datetime.combine(now, time=time(8,59,50)))
+# except Exception as e:
+#     print(type(e).__name__, ":", e)
+print(datetime.strftime(now, '%H:%M'))
